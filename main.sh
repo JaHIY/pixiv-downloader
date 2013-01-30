dfa803cdc1a73b05a6d46ca3053803b71d9a0c9a
JaHIY <jaklsy@gmail.com>
2013-01-30 21:02:31 +0800

    make some variable local

--- main.sh
+++ main.sh
@@ -2,18 +2,13 @@
 
 . ./config
 
-PIXIV_IMG_ID_REGEX='^.*illust_id=\([[:digit:]]\+\).*$'
+
 COOKIE_FILE='cookie.txt'
 USER_AGENT='Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1'
 PIXIV_MEDIUM_PREFIX='http://www.pixiv.net/member_illust.php?mode=medium&illust_id='
 PIXIV_BIG_PREFIX='http://www.pixiv.net/member_illust.php?mode=big&illust_id='
 PIXIV_MANGA_PREFIX='http://www.pixiv.net/member_illust.php?mode=manga&illust_id='
 PIXIV_MANGA_BIG_PREFIX='http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id='
-PIXIV_SINGLE_IMG_REGEX='http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+\.[[:alpha:]]\+'
-PIXIV_MODE_SEARCH='class="works_display"'
-PIXIV_MODE_REGEX='^.*class="works_display"><a href="member_illust.php?mode=\([^&]\+\).*$'
-PIXIV_MANGA_IMG_REGEX='http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+_p[[:digit:]]\+\.[[:alpha:]]\+'
-PIXIV_MANGA_IMG_SUBSTITUTE='s;^\(http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+\)\(_p[[:digit:]]\+\.[[:alpha:]]\+\)$;\1_big\3;g'
 
 get_cookie() {
     printf 'Now I am getting cookie......'
@@ -22,14 +17,19 @@ get_cookie() {
 }
 
 check_mode() {
+    local PIXIV_MODE_SEARCH='class="works_display"'
+    local PIXIV_MODE_REGEX='^.*class="works_display"><a href="member_illust.php?mode=\([^&]\+\).*$'
     curl -s -b "$COOKIE_FILE" -A "$USER_AGENT" "${PIXIV_MEDIUM_PREFIX}${1}" | grep -F "$PIXIV_MODE_SEARCH" | sed -e "s/${PIXIV_MODE_REGEX}/\1/"
 }
 
 get_pixiv_img_id() {
+    local PIXIV_IMG_ID_REGEX='^.*illust_id=\([[:digit:]]\+\).*$'
     sed -e "s/${PIXIV_IMG_ID_REGEX}/\1/" <<< "$1"
 }
 
 download_pixiv_manga_imgs() {
+    local PIXIV_MANGA_IMG_REGEX='http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+_p[[:digit:]]\+\.[[:alpha:]]\+'
+    local PIXIV_MANGA_IMG_SUBSTITUTE='s;^\(http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+\)\(_p[[:digit:]]\+\.[[:alpha:]]\+\)$;\1_big\3;g'
     curl -s -b "$COOKIE_FILE" -A "$USER_AGENT" -e "${PIXIV_MEDIUM_PREFIX}${1}" "${PIXIV_MANGA_PREFIX}${1}" | grep -o "$PIXIV_MANGA_IMG_REGEX" | sed -e "$PIXIV_MANGA_IMG_SUBSTITUTE" | \
     while read pixiv_img_url
     do
@@ -38,6 +38,7 @@ download_pixiv_manga_imgs() {
 }
 
 download_pixiv_single_img() {
+    local PIXIV_SINGLE_IMG_REGEX='http:\/\/[^.]\+\.pixiv\.net\/\([^/]\+\/\)\{3\}[[:digit:]]\+\.[[:alpha:]]\+'
     local pixiv_img_url=$(curl -s -b "$COOKIE_FILE" -A "$USER_AGENT" -e "${PIXIV_MEDIUM_PREFIX}${1}" "${PIXIV_BIG_PREFIX}${1}" | grep -o "$PIXIV_SINGLE_IMG_REGEX")
     curl -O -b "$COOKIE_FILE" -A "$USER_AGENT" -e "${PIXIV_BIG_PREFIX}${1}" "$pixiv_img_url"
 }
